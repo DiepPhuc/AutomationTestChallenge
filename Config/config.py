364414370be8e4e62f712083fe4ba0ad5f1f35ce
DiepPhuc <56315134+DiepPhuc@users.noreply.github.com>
2020-11-03 16:30:07 +0700

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,5 +1,5 @@
 class TestData:
-    BASE_URL = 'http://ktvn-test.s3-website.us-east-1.amazonaws.com'
+    BASE_URL = 'domainURL'
     USER_NAME = 'admin@test.com'
     PASSWORD = 'test123'
     INACTIVE_STATUS = 'Inactive'
